43b8bb0930a11e0e079e1171297ac31814330f55
florencialp <f.lopez.pacini@gmail.com>
2021-09-05 23:30:15 -0300

    1

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     instagram: {
         clientId: '434732221251949',
-        clientSecret: 'IGQVJYN1BpTjA2TDZAmS2pPNVAtNG5IbGxyUWdBNnRmU3Rqdkh2Umk2cWlYTmpnTEpFc2gtR2hzV2N2aTYwZAC1HbTh6YVdzc2drejZAZAOXBTaWk4R0RRUUFDZAzhUR0YxeWtrOTlYZAW5jU3FpYVp4TUhhMwZDZD',
+        clientSecret: 'e19168c57440e4dd820a2d7d31a620af',
         accessToken: 'IGQVJYN1BpTjA2TDZAmS2pPNVAtNG5IbGxyUWdBNnRmU3Rqdkh2Umk2cWlYTmpnTEpFc2gtR2hzV2N2aTYwZAC1HbTh6YVdzc2drejZAZAOXBTaWk4R0RRUUFDZAzhUR0YxeWtrOTlYZAW5jU3FpYVp4TUhhMwZDZD',
     }   
 }
