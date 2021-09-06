903335a8e9b51321aa82d234da1013211f7f4950
florencialp <f.lopez.pacini@gmail.com>
2021-09-05 23:35:20 -0300

    2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,48 +1,72 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-// api instagram
-const Instagram = require('node-instagram').default; // módulo
-const { clientId, clientSecret } = require('../keys').instagram;
+// Instagram API
+const Instagram = require("node-instagram").default;
+const { clientId, clientSecret } = require("../keys").instagram;
 
-const instagram = new Instagram({ // instancia del módulo
-    clientId: clientId,
-    clientSecret: clientSecret
-})
+const instagram = new Instagram({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
+const redirectUri = "https://pagina-prueba1.herokuapp.com//handleauth";
 
-const redirectUri = 'http://localhost:3000/handleauth';
-router.get('/auth/instagram', (req, res) => {
-    console.log('hiii');
-    res.redirect(
-        instagram.getAuthorizationUrl(redirectUri, {
-            scope: ['basic', 'likes'], // en scope está lo que quiero obtener de instagram
-            state: 'your state'
-        })
-    );
+// First redirect user to instagram oauth
+router.get("/auth/instagram", (req, res) => {
+  res.redirect(
+    instagram.getAuthorizationUrl(redirectUri, {
+      // an array of scopes
+      scope: ["basic", "likes"],
+      // an optional state
+      state: "your state"
+    })
+  );
 });
 
-router.get('/', (req, res) => {
-    res.render('index');
-});
-
-
-
-router.get('/handleauth', async (req, res) => {
+// Handle auth code and get access_token for user
+router.get("/handleauth", async (req, res) => {
+  try {
+    // The code from the request, here req.query.code for express
     const code = req.query.code;
-    const data = await instagram.getAuthorizeUser(code, redirectUri);
-    console.log(data);
+    const data = await instagram.authorizeUser(code, redirectUri);
+    // data.access_token contain the user access_token
+    // res.json(data);
+    // console.log(data);
+    // Store login data in a session
+    req.session.access_token = data.access_token;
+    req.session.user_id = data.user.id;
+
+    instagram.config.accessToken = req.session.access_token;
+    console.log(instagram);
+    res.redirect("/profile");
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+router.get("/profile", async (req, res) => {
+  try {
+    const profileData = await instagram.get("users/self");
+    const media = await instagram.get('users/self/media/recent');
+    console.log(profileData);
+    res.render("profile", { user: profileData.data, posts: media.data});
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 router.get("/login", (req, res) => {
-    res.redirect("/auth/instagram");
+  res.redirect("/auth/instagram");
 });
 
-router.get('/logout'), () => {
-
-};
-
-router.get('/profile'), () => {
-
-};
+router.get("/logout", (req, res) => {
+  delete req.session.access_token;
+  delete req.session.user_id;
+  res.redirect("/");
+});
 
 module.exports = router;
