95964631f9bcc13a906c2d48ad71be4c551c744f
florencialp <f.lopez.pacini@gmail.com>
2021-09-12 17:45:00 -0300

    34

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 require('dotenv').config();
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
 
 // Instagram API
